3b991021a365ac8eeeb48edda5d2ed8a8045e754
krlaan <kristjan.laanet@gmail.com>
2026-02-19 15:47:36 +0200

    Add backend entities - Room and BookedRoom

--- hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/model/BookedRoom.java
+++ hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/model/BookedRoom.java
@@ -1,4 +1,67 @@
 package com.krlaan.hotelbooking.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookedRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int bookingId;
+
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_fullname")
+    private String guestFullName;
+
+    @Column(name = "guest_email")
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private int NumOfAdults;
+
+    @Column(name = "children")
+    private int NumOfChildren;
+
+    @Column(name = "total_guest")
+    private int totalNumOfGuest;
+
+    @Column(name = "confirmation_code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumOfGuest() {
+        this.totalNumOfGuest = this.NumOfAdults + this.NumOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        NumOfAdults = numOfAdults;
+        calculateTotalNumOfGuest(); // if guest changes the number of people
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        NumOfChildren = numOfChildren;
+        calculateTotalNumOfGuest(); // if guest changes the number of people
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
--- hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/model/Room.java
+++ hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/model/Room.java
@@ -1,4 +1,48 @@
 package com.krlaan.hotelbooking.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String roomType;
+    private double price;
+    private boolean isBooked = false;
+
+    @Lob
+    private Blob photo;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings;
+
+    public Room() { // Avoid NullPointerException, Initialize bookings list
+        this.bookings = new ArrayList<>();
+    }
+
+    public void addBooking(BookedRoom booking) {
+        if (bookings == null) {
+            bookings = new ArrayList<>();
+        }
+        bookings.add(booking);
+        booking.setRoom(this); // Ensure the booking knows its parent Room
+        isBooked = true;
+
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        booking.setBookingConfirmationCode(bookingCode);
+    }
 }
