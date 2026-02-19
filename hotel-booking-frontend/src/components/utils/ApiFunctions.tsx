356ad47451febcedcf7a2532902718ae58410efe
krlaan <kristjan.laanet@gmail.com>
2026-02-19 21:43:55 +0200

    Add addRoom and getRoomTypes API functions

--- hotel-booking-frontend/src/components/utils/ApiFunctions.tsx
+++ hotel-booking-frontend/src/components/utils/ApiFunctions.tsx
@@ -4,3 +4,25 @@ export const api = axios.create({
     baseURL: 'http://localhost:9192',
 })
 
+// This function add a new room to the database
+export async function addRoom(photo: File, roomType: string, roomPrice: number) {
+    const formData = new FormData();
+    formData.append('photo', photo);
+    formData.append('roomType', roomType);
+    formData.append('roomPrice', roomPrice.toString());
+
+    const response = await api.post('/rooms/add/new-room', formData)
+
+    return response.status === 201; // true or false
+}
+
+// This function gets all room types from the database
+export async function getRoomTypes() {
+    try {
+        const response = await api.get('/rooms/room-types');
+        return response.data;
+
+    } catch {
+         throw new Error("Error fetching room types");
+    }
+}
