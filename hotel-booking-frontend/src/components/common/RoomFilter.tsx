5a28ac5086f0b9b18b751d74628b688609d79e52
krlaan <kristjan.laanet@gmail.com>
2026-02-20 15:18:21 +0200

    Add RoomFilter component UI

--- hotel-booking-frontend/src/components/common/RoomFilter.tsx
+++ hotel-booking-frontend/src/components/common/RoomFilter.tsx
@@ -35,8 +35,27 @@ const RoomFilter = ({data, setFilteredData}: Props) => {
     const roomTypes = ["", ...new Set(data.map(room => room.roomType))];
 
     return (
-        <div>
-
+        <div className="input-group mb-3">
+            <span className="input-group-text" id="room-type-filter">
+                Filter rooms by type
+            </span>
+            <select
+                className="form-select"
+                value={filter}
+                onChange={handleSelectChange}
+            >
+                <option value={""}>
+                    Select a room type to filter...
+                </option>
+                {roomTypes.map((type, index) => (
+                    <option key={index} value={type}>
+                        {type}
+                    </option>
+                ))}
+            </select>
+            <button className="btn btn-hotel" type="button" onClick={clearFilter}>
+                Clear Filter
+            </button>
         </div>
     );
 };
