60468197879f7220d3441da69716555c694e21ae
krlaan <kristjan.laanet@gmail.com>
2026-02-20 22:27:32 +0200

    feat (frontend): Add Navbar and fix footer

--- hotel-booking-frontend/src/App.tsx
+++ hotel-booking-frontend/src/App.tsx
@@ -4,20 +4,24 @@ import Rooms from "./components/room/Rooms.tsx";
 import Home from "./components/home/Home.tsx";
 import EditRoom from "./components/room/EditRoom.tsx";
 import AddRoom from "./components/room/AddRoom.tsx";
+import NavBar from "./components/layout/NavBar.tsx";
+import Footer from "./components/layout/Footer.tsx";
 
 function App() {
   return (
     <>
-        <main>
-            <Router>
+        <Router>
+            <NavBar />
+            <main>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/edit-room/:roomId" element={<EditRoom />} />
                     <Route path="/rooms" element={<Rooms />} />
                     <Route path="/add-room" element={<AddRoom />} />
                 </Routes>
-            </Router>
-        </main>
+            </main>
+            <Footer />
+        </Router>
     </>
   )
 }
