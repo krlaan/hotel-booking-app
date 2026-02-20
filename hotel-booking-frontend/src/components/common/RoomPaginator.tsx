2ba82a8b0304fd69e9d9faf5defaa5744fbb3eb8
krlaan <kristjan.laanet@gmail.com>
2026-02-20 14:55:26 +0200

    Add room pagination file

---
+++ hotel-booking-frontend/src/components/common/RoomPaginator.tsx
@@ -0,0 +1,30 @@
+type Props = {
+    currentPage: number;
+    totalPages: number;
+    onPageChange: (e: number) => void;
+};
+
+const RoomPaginator = ({currentPage, totalPages, onPageChange}: Props) => {
+    const pageNumbers = Array.from({length : totalPages}, (_, i) => i + 1);
+
+    return (
+        <nav aria-label="Page navigation">
+            <ul className="paginator justify-content-center">
+                {pageNumbers.map((pageNumber) => (
+                    <li
+                        key={pageNumber}
+                        className={`page-item ${pageNumber === currentPage ? 'active' : ''}`}>
+                        <button
+                            className="page-link"
+                            onClick={() => onPageChange(pageNumber)}
+                        >
+                            {pageNumber}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export default RoomPaginator;
