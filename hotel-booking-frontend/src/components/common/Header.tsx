9c44f0a9dcb8d08f63714e087146a61afa486f60
krlaan <kristjan.laanet@gmail.com>
2026-02-22 15:30:25 +0200

    Add components for Home page

---
+++ hotel-booking-frontend/src/components/common/Header.tsx
@@ -0,0 +1,15 @@
+const Header = (title: string) => {
+    return (
+        <header className="header">
+            <div className="overlay"></div>
+            <h1>
+                Welcome to <span className="hotel-color">Hotel Booking System</span>
+            </h1>
+            <div className="container">
+                <h1 className="header-title text-center">{title}</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ hotel-booking-frontend/src/components/layout/HeaderMain.tsx
@@ -0,0 +1,16 @@
+const HeaderMain = () => {
+    return (
+        <header className="header-banner">
+            <div className="overlay">
+            </div>
+            <div className="animated-texts overlay-content">
+                <h1>
+                    Welcome to <span className="hotel-color">Hotel Booking System</span>
+                </h1>
+                <h4>Experience the Best Hotel in Town!</h4>
+            </div>
+        </header>
+    );
+};
+
+export default HeaderMain;
