1b0f08cfbb5847cc9ac1074cfc683746e20decd6
krlaan <kristjan.laanet@gmail.com>
2026-02-25 20:46:51 +0200

    Add JwtUtils for JWT generation and validation

---
+++ hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/security/jwt/JwtUtils.java
@@ -0,0 +1,72 @@
+package com.krlaan.hotelbooking.security.jwt;
+
+import com.krlaan.hotelbooking.security.user.HotelUserDetails;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+public class JwtUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${security.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${security.jwt.expirationTime}")
+    private int jwtExpirationTime;
+
+    public String generateJwtTokenForUser(Authentication authentication) {
+        HotelUserDetails userPrincipal = (HotelUserDetails) authentication.getPrincipal();
+
+        List<String> roles = userPrincipal.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        return Jwts.builder().setSubject(userPrincipal.getUsername())
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationTime))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(token);
+            return true;
+
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token : {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("Expired JWT token : {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("Unsupported JWT token : {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims found : {}", e.getMessage());
+        }
+
+        return false;
+    }
+}
