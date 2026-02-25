3a076e79964d6ee8dea225ff272328176cf1ab9e
krlaan <kristjan.laanet@gmail.com>
2026-02-25 20:24:20 +0200

    Add security for user details

---
+++ hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/security/user/HotelUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.krlaan.hotelbooking.security.user;
+
+import com.krlaan.hotelbooking.model.User;
+import com.krlaan.hotelbooking.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HotelUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+
+        return HotelUserDetails.buildUserDetails(user);
+    }
+}
