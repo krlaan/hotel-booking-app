18bab77c5f29a2750642961e81bd0328abef7a48
krlaan <kristjan.laanet@gmail.com>
2026-02-25 18:42:19 +0200

    Add initial user controller

---
+++ hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.krlaan.hotelbooking.controller;
+
+import com.krlaan.hotelbooking.model.User;
+import com.krlaan.hotelbooking.service.IUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final IUserService userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getUsers() {
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<?> getUserByEmail(@PathVariable("email") String email) {
+        try {
+            User user = userService.getUser(email);
+            return ResponseEntity.ok(user);
+
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching users");
+        }
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable("userId") String email) {
+        try {
+            userService.deleteUser(email);
+            return ResponseEntity.ok("User deleted successfully");
+
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting users");
+        }
+    }
+}
--- hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/service/UserService.java
+++ hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/service/UserService.java
@@ -9,11 +9,12 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+@Service
 @RequiredArgsConstructor
 public class UserService implements IUserService {
 
