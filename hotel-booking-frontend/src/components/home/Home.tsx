86ae8e967d867a569523b6e240ff1386d9514146
krlaan <kristjan.laanet@gmail.com>
2026-02-25 16:54:34 +0200

    Add room search on Home page

--- hotel-booking-frontend/src/components/home/Home.tsx
+++ hotel-booking-frontend/src/components/home/Home.tsx
@@ -2,6 +2,7 @@ import MainHeader from "../layout/MainHeader.tsx";
 import HotelService from "../common/HotelService.tsx";
 import Parallax from "../common/Parallax.tsx";
 import RoomCarousel from "../common/RoomCarousel.tsx";
+import RoomSearch from "../common/RoomSearch.tsx";
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <MainHeader />
 
             <section className="container">
+                <RoomSearch />
                 <RoomCarousel />
                 <Parallax />
                 <RoomCarousel />
