0e4c5963eaab0b6b742c12dafd2127fe3cd438b7
krlaan <kristjan.laanet@gmail.com>
2026-02-25 20:58:17 +0200

    Add JWT authentication filter (AuthTokenFilter)

--- hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/security/jwt/JwtUtils.java
+++ hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/security/jwt/JwtUtils.java
@@ -9,11 +9,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.List;
 
+@Component
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
