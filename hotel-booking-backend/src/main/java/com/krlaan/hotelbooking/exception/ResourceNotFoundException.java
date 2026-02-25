6b6fb41a238ec1e379aae356f7077e7799c23088
krlaan <kristjan.laanet@gmail.com>
2026-02-25 18:25:26 +0200

    Add new security dependency and Change throwable error in custom error

--- hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/exception/ResourceNotFoundException.java
+++ hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/exception/ResourceNotFoundException.java
@@ -1,6 +1,6 @@
 package com.krlaan.hotelbooking.exception;
 
-public class ResourceNotFoundException extends Throwable {
+public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
     }
