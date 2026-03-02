0cafe25a45163462a635e9d8e3c0d20df8de8b23
krlaan <kristjan.laanet@gmail.com>
2026-03-02 16:08:48 +0200

    Delete unnecessary method in BookingRepository

--- hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/repository/BookingRepository.java
+++ hotel-booking-backend/src/main/java/com/krlaan/hotelbooking/repository/BookingRepository.java
@@ -8,8 +8,6 @@ import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<BookedRoom, Long> {
 
-    List<BookedRoom> findByRoomId(Long roomId);
-
     Optional<BookedRoom> findByBookingConfirmationCode(String confirmationCode);
 
     List<BookedRoom> findByGuestEmail(String email);
